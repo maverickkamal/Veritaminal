56127d043d09d06174a4a77ac7aa524a5b8ee0b1
maverickkamal <musakamalmuhammad@gmail.com>
2025-03-15 22:45:28 +0100

    Bump version to 1.0.1

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ A game where players act as border control agents, verifying AI-generated
 documents to approve or deny travelers.
 """
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from game.main import main
 
